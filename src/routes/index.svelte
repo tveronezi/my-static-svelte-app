0e757fe934a9b5be897ca4defcc99d291b479296
Thiago Veronezi <tveronezi@gmail.com>
2022-03-15 12:00:41 -0400

    init

---
+++ src/routes/index.svelte
@@ -0,0 +1,11 @@
+<script context="module" lang="ts">
+	export const prerender = true;
+</script>
+
+<section>
+	<a href="/a">a</a>
+	<a href="/b">b</a>
+	<a href="/c">c</a>
+	<a href="/d">d</a>
+	<a href="/e">e</a>
+</section>
